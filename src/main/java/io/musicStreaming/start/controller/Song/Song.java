0e7cdbf95b83b9191a973f668c6a6c9b1d25766b
yasseryka <yasseryka@gmail.com>
2019-11-28 07:56:54 -0500

    made song class pojo and used hibernate's annotations on each of it attribute

--- src/main/java/io/musicStreaming/start/controller/Song/Song.java
+++ src/main/java/io/musicStreaming/start/controller/Song/Song.java
@@ -1,11 +1,51 @@
 package io.musicStreaming.start.controller.Song;
 
+//@Entity
+//@Table(name = "SONG")
 public class Song {
 
+	//@Id @GeneratedValue
+	//@Column(name ="id")
 	private int id;
+	//@Column(name ="title")
 	private String title;
+	//@Column(name ="artistId")
 	private int artistId;
+	//@Column(name ="albumId")
 	private int albumId;
+	//@Column(name ="url")
 	private String url;
 	
+	public Song() {}
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public int getArtistId() {
+		return artistId;
+	}
+	public void setArtistId(int artistId) {
+		this.artistId = artistId;
+	}
+	public int getAlbumId() {
+		return albumId;
+	}
+	public void setAlbumId(int albumId) {
+		this.albumId = albumId;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
 }
--- src/main/java/io/musicStreaming/start/controller/Song/SongController.java
+++ src/main/java/io/musicStreaming/start/controller/Song/SongController.java
@@ -1,13 +1,15 @@
 package io.musicStreaming.start.controller.Song;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SongController {
 
-	@RequestMapping("/song")
-	public String getSongs() {
-		return "Successful!";
+	@RequestMapping("/songs")
+	public List<Song> getSongs() {
+		return null;
 	}
 }
