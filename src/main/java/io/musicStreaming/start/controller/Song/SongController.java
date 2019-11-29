1c5f0d37686934629e4fbb1c9a628757f089267a
yasseryka <yasseryka@gmail.com>
2019-11-29 12:50:47 -0500

    added properties file and thymleaf and thymeleaf cache propertie to true

--- src/main/java/io/musicStreaming/start/controller/Song/SongController.java
+++ src/main/java/io/musicStreaming/start/controller/Song/SongController.java
@@ -2,14 +2,21 @@ package io.musicStreaming.start.controller.Song;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("songs")
 public class SongController {
 
-	@RequestMapping("/songs")
+	@GetMapping
 	public List<Song> getSongs() {
+		return null;
+	}
+	
+	@RequestMapping("songs/{id}")
+	public Song getSong() {
 		return null;
 	}
 }
