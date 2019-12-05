86120ab673b65d8f457f4a5262d0453c16197912
yasseryka <yasseryka@gmail.com>
2019-12-05 13:53:59 -0500

    fixes last commit's error that happens in build time in Song's class by changing field's column name into this style (userId -> user_id)

--- src/main/java/io/musicStreaming/start/Model/Song.java
+++ src/main/java/io/musicStreaming/start/Model/Song.java
@@ -3,32 +3,22 @@ package io.musicStreaming.start.Model;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Song {
 
-	@Id @GeneratedValue
-	private Long id;
-	@Column(nullable = false)
+	@Id @GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	@Column(nullable = false, name="title")
 	private String title;
-	@Column(nullable = false)
+	@Column(nullable = false, name="artist_Id")
 	private int artistId;
-	@Column(nullable = false)
+	@Column(nullable = false, name="album_Id")
 	private int albumId;
-	@Column(nullable = false)
+	@Column(nullable = false, name="Url")
 	private String url;
-	
-	public Song() {}
-	
-	public Song(Long id, String title, int artistId, int albumId, String url) {
-		super();
-		this.id = id;
-		this.title = title;
-		this.artistId = artistId;
-		this.albumId = albumId;
-		this.url = url;
-	}
 
 	public Long getId() {
 		return id;
--- target/classes/data.sql
+++ target/classes/data.sql
@@ -1,1 +1,1 @@
-INSERT INTO Song (title, artistId, albumId, url) VALUES ('Antidepresseur', 1, 1, 'Antidepresseur.mp3');
+INSERT INTO song (title, artist_id, album_id, url) VALUES ('Antidepresseur', 1, 1, 'Antidepresseur.mp3');
