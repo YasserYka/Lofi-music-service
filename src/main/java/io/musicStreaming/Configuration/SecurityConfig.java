3f044ec817a42e7e43d4c0cbec32bddef69f7f22
yasseryka <yasseryka@gmail.com>
2020-01-13 13:55:44 -0500

    added userdetailsservice and userdetails classes and changed config.authbuilder in securityconfig

--- src/main/java/io/musicStreaming/Configuration/SecurityConfig.java
+++ src/main/java/io/musicStreaming/Configuration/SecurityConfig.java
@@ -8,7 +8,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -18,14 +18,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	@Autowired
 	DataSource dataSource;
 	
+	@Autowired
+	UserDetailsService userDetailsService;
+	
 	@Override
-	protected void configure(AuthenticationManagerBuilder authentication) {
-		try {
-			authentication.jdbcAuthentication()
-			.dataSource(dataSource);
-		}
-		catch (Exception e) {/*TODO: log it*/ e.printStackTrace();}
-	}
+	protected void configure(AuthenticationManagerBuilder authentication) throws Exception {authentication.userDetailsService(userDetailsService);}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
---
+++ src/main/java/io/musicStreaming/Configuration/userDetails.java
@@ -0,0 +1,54 @@
+package io.musicStreaming.Configuration;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class userDetails implements UserDetails{
+
+	private String username;
+
+	public userDetails() {}
+	
+	public userDetails(String username) {
+		this.username = username;
+	}
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"));
+	}
+
+	@Override
+	public String getPassword() {return "password";}
+
+	@Override
+	public String getUsername() {return username;}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+}
