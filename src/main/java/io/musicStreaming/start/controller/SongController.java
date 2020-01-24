389ba94434e2532e0c3d778a87fde1532ae6ce6e
yasseryka <yasseryka@gmail.com>
2020-01-24 14:55:22 -0500

    handled add song and modified related files

--- src/main/java/io/musicStreaming/start/controller/SongController.java
+++ src/main/java/io/musicStreaming/start/controller/SongController.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -54,11 +55,19 @@ public class SongController {
 	}
 
 	@PostMapping("/upload")
-	public String handleUpload(@RequestParam("audio") MultipartFile audio, @RequestParam("image") MultipartFile image, RedirectAttributes redirect) {
+	public String handleUpload(@RequestParam("audio") MultipartFile audio, @RequestParam("image") MultipartFile image, RedirectAttributes redirect, @RequestParam String title, @RequestParam String artistName) {
 		if(audio.isEmpty() || image.isEmpty())
 			redirect.addFlashAttribute("message", "Please make sure to upload both audio and image");
 		else
 			redirect.addFlashAttribute("message", "Thank you for uploading");
+		
+		Song song = new Song();
+		song.setTitle(title);
+		song.setArtistName(artistName);
+		song.setImageUrl("/images/"+ image.getOriginalFilename());
+		song.setAudioUrl("/audio/"+ audio.getOriginalFilename());
+		
+		repository.save(song);
 		
 		fileService.uploadFileOfKind(audio, "audio");
 		fileService.uploadFileOfKind(image, "image");
