ed46a725991e2a654a06d297056cdf01894b2c49
yasseryka <yasseryka@gmail.com>
2020-01-25 07:21:30 -0500

    moved every repo interaction to songservice

--- src/main/java/io/musicStreaming/start/controller/SongController.java
+++ src/main/java/io/musicStreaming/start/controller/SongController.java
@@ -1,25 +1,12 @@
 package io.musicStreaming.start.controller;
 
-import io.musicStreaming.start.exception.SongNotFoundException;
 import io.musicStreaming.start.model.Song;
-import io.musicStreaming.start.repository.SongsRepository;
 import io.musicStreaming.start.service.FileService;
 import io.musicStreaming.start.service.SongService;
 
-import java.io.IOException;
-import java.lang.ProcessBuilder.Redirect;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -50,38 +37,32 @@ public class SongController {
 	}
 
 
-	/*@GetMapping("/song/{id}")
+	@GetMapping("/song/{id}")
 	public String getSong(Model model, @PathVariable Long id) {
-		model.addAttribute("song", repository.findById(id).orElseThrow(() ->new SongNotFoundException(id)));
+		model.addAttribute("song", songService.findById(id));
 		return "play";
-	}*/
+	}
 	
 	@GetMapping("/upload")
 	public String upload(){
 		return "upload";
 	}
 
-	/*@PostMapping("/upload")
+	@PostMapping("/upload")
 	public String handleUpload(@RequestParam("audio") MultipartFile audio, @RequestParam("image") MultipartFile image, RedirectAttributes redirect, @RequestParam String title, @RequestParam String artistName) {
 		if(audio.isEmpty() || image.isEmpty())
 			redirect.addFlashAttribute("message", "Please make sure to upload both audio and image");
 		else
 			redirect.addFlashAttribute("message", "Thank you for uploading");
 		
-		Song song = new Song();
-		song.setTitle(title);
-		song.setArtistName(artistName);
-		song.setImageUrl("/images/"+ image.getOriginalFilename());
-		song.setAudioUrl("/songs/"+ audio.getOriginalFilename());
-		
-		repository.save(song);
+		songService.saveSong(title, artistName, image, audio);
 		
 		fileService.uploadFileOfKind(audio, "audio");
 		fileService.uploadFileOfKind(image, "image");
 		
 		return "redirect:/uploadStatus";
 	}
-*/
+
 	
 	@GetMapping("/uploadStatus")
 	public String uploadStatus() {return "uploadStatus";}
--- src/main/java/io/musicStreaming/start/service/SongService.java
+++ src/main/java/io/musicStreaming/start/service/SongService.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import io.musicStreaming.start.exception.SongNotFoundException;
 import io.musicStreaming.start.model.Song;
 import io.musicStreaming.start.repository.SongsRepository;
 
@@ -16,5 +18,20 @@ public class SongService {
 	
 	public Page<Song> songsList(int size, int page){	
 		return repository.findAll(PageRequest.of(page, size));
+	}
+	
+	public Song findById(Long id) {
+		return repository.findById(id).orElseThrow(() ->new SongNotFoundException(id));
+	}
+
+	public void saveSong(String title, String artistName, MultipartFile image, MultipartFile audio) {
+		
+		Song song = new Song();
+		song.setTitle(title);
+		song.setArtistName(artistName);
+		song.setImageUrl("/images/"+ image.getOriginalFilename());
+		song.setAudioUrl("/songs/"+ audio.getOriginalFilename());
+		
+		repository.save(song);
 	}
 }
