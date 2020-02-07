ad2a398f1a3ac0511f8771076c2ed162768a5f90
yasseryka <yasseryka@gmail.com>
2020-02-07 03:55:51 -0500

    finished frontend of profile end point

--- target/classes/static/js/profile.js
+++ target/classes/static/js/profile.js
@@ -4,7 +4,6 @@ window.onload = getInformation;
 
 function getInformation(){
     const token = localStorage.getItem('token');
-    console.log(token);
     if(token){
         fetch(baseUrl+"/user", {
             method: "GET",
@@ -13,10 +12,23 @@ function getInformation(){
         .then(response => response.json())
         .then(data => {
             /*if(!data.message)
-                //Something went wrong probably
+                //Something went wrong probably jwt expired?
                 localStorage.removeItem("token");
-            else*/
-                console.log(data);
+            else
+            console.log(data);*/
+            constructProfile(data)
         })
     }
+}
+
+function constructProfile(data){
+    document.getElementById("name").innerHTML = data.name;
+    document.getElementById("username").innerHTML = data.username;
+    document.getElementById("signupDate").innerHTML = data.signupDate;
+    let imageContainer = document.getElementById("image-container");
+
+    let image = document.createElement("img");
+    image.id = "image";
+    image.src = data.imageUrl;
+    imageContainer.appendChild(image);
 }
